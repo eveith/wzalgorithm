b5fcb04a1dc4a4e4b299702181929b6e0b14f1c4
Eric MSP Veith <eveith@gnyu-linux.org>
2015-07-28 11:53:02 +0200

    Started separate algorithm library

---
+++ src/algorithm/REvol.h
@@ -0,0 +1,21 @@
+#ifndef WINZENT_ALGORITHM_REVOL_H_
+#define WINZENT_ALGORITHM_REVOL_H_
+
+
+namespace Winzent {
+    namespace Algorithm {
+
+
+        /*!
+         * \brief The REvol class implements an optimization algorithm that
+         *  is based on REvol.
+         */
+        class REvol
+        {
+
+        };
+    }
+}
+
+
+#endif // WINZENT_ALGORITHM_REVOL_H_
