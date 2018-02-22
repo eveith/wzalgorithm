ab34366569c781eea7bcc4c83122d0a3c65d06a7
Eric MSP Veith <eric.veith@offis.de>
2018-02-22 17:04:51 +0100

    Better CMakeLists.txt for SWIG+Python, unit test for Python wrapper

--- test/REvolTest.cpp
+++ test/REvolTest.cpp
@@ -97,16 +97,12 @@ TEST(REvolTest, testCrossInTray)
     REvol revol;
     revol.maxEpochs(5000);
 
-    Individual origin;
-    origin.scatter = {1, 1};
-    origin.parameters = {-10.0, 10.0};
-
     auto succeeds = [](Individual& i) {
         i.restrictions[0] = ::crossInTray(i.parameters[0], i.parameters[1]);
         return i.restrictions[0] < -2.062;
     };
 
-    auto result = revol.run(origin, succeeds);
+    auto result = revol.run(revol.generateOrigin(2), succeeds);
 
     ASSERT_TRUE(::crossInTray(result.parameters[0], result.parameters[1])
             < -2.0);
--- test/test_wzalgorithm.py
+++ test/test_wzalgorithm.py
@@ -16,17 +16,23 @@ class CrossinTrayEvaluator:
                 + 1), \
                 0.1)
 
-    def __init__(self):
-        print("Welcome!")
-
     def __call__(self, individual):
         individual.restrictions.resize(1)
         individual.restrictions[0] = CrossinTrayEvaluator.crossin_tray(individual.parameters[0],
                                                                        individual.parameters[1])
         return individual.restrictions[0] < -2.07
 
-    def __del__(self):
-        print("Good-bye says the evaluator.")
+
+class EggholderEvaluator:
+    def eggholder(x1, x2):
+        return -(x2 + 47) * math.sin(math.sqrt(abs(x2 + 0.5 * x1 +47))) \
+            - x1 * math.sin(math.sqrt(abs(x1 - (x2 + 47))))
+
+    def __call__(self, individual):
+        individual.restrictions.resize(1)
+        individual.restrictions[0] = EggholderEvaluator.eggholder(individual.parameters[0],
+                                                                  individual.parameters[1])
+        return individual.restrictions[0] < -959
 
 
 class TestREvol(unittest.TestCase):
@@ -34,7 +40,21 @@ class TestREvol(unittest.TestCase):
         revol = wzalgorithm.REvol()
         origin = revol.generateOrigin(2)
 
-        revol.maxEpochs(5000)
+        revol.maxEpochs(10000)
         result = revol.runPredicated(origin,
                                      wzalgorithm.REvolSuccessPredicate(CrossinTrayEvaluator()))
-        print("Result is: {}".format(result.error))
+        self.assertTrue(result.error < -2.07)
+
+    def test_eggholder(self):
+        revol = wzalgorithm.REvol()
+        origin = revol.generateOrigin(2)
+
+        revol.maxEpochs(10000)
+        result = revol.runPredicated(origin,
+                                     wzalgorithm.REvolSuccessPredicate(EggholderEvaluator()))
+        self.assertTrue(result.error < -959)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
