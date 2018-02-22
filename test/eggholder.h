9c62a79626e57d85e85b53e02c293c2ab7669028
Eric MSP Veith <eric.veith@offis.de>
2018-02-22 16:50:36 +0100

    Added "eggholder" test function

--- test/eggholder.h
+++ test/eggholder.h
@@ -1,4 +1,20 @@
 #ifndef EGGHOLDER_H
 #define EGGHOLDER_H
 
+
+#include <cmath>
+
+
+using std::abs;
+using std::sin;
+using std::sqrt;
+
+
+static double eggholder(double x1, double x2)
+{
+    return -(x2 + 47.) * sin(sqrt(abs(x2 + x1/2. + 47)))
+            - x1 * sin(sqrt(abs(x1 - (x2 + 47.))));
+}
+
+
 #endif // EGGHOLDER_H
