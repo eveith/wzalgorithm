90f36347c5b33389f8bcb458407f726a6ca8500f
Eric MSP Veith <eric.veith@offis.de>
2019-05-15 13:33:59 +0200

    Fixed silly mistake in crossintray function

--- test/crossin.h
+++ test/crossin.h
@@ -20,7 +20,7 @@ static double crossInTray(double x, double y)
                 * sin(y)
                 * exp(
                     abs(
-                        100 - sqrt(pow(x, 2) * pow(y, 2))/ M_PI)))
+                        100 - sqrt(pow(x, 2) + pow(y, 2))/ M_PI)))
             + 1),
             0.1);
 }
